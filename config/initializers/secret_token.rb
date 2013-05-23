59d19b1d89f48f7bb17ad1c83c5b08a7f8e3f81b
Mikahil Gerasimov <GerasimovMN@khv.dv.rt.ru>
2013-05-24 08:00:57 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lettermail::Application.config.secret_token = '27cd47f79e402b3959751d31a9f1a8bd2dd53ebb21917edc66159481b96366242545ef0b63969f0e427cddbc9d58b8f142e2b9a2a711ea97a02f753facd4dd05'
